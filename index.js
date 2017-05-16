7e5c7a1a6f5607726480a1416490082b7e53e84f
Iftekher Sunny <iftekhersunny@hotmail.com>
2017-05-16 09:46:35 +0600

    Reformat

--- index.js
+++ index.js
@@ -27,28 +27,28 @@ messages = validateOnLoad(messages)
  */
 function validateTitle(messages) {
 
-	// validating title
-	if($('title')[0].innerHTML.length > 55) {
-			messages.push("Title langth must be less than 55 character")
-	}
+    // validating title
+    if ($('title')[0].innerHTML.length > 55) {
+        messages.push("Title langth must be less than 55 character")
+    }
 
-	if($('title')[0].innerHTML.length < 1) {
-			messages.push("Title can not be empty")
-	}
+    if ($('title')[0].innerHTML.length < 1) {
+        messages.push("Title can not be empty")
+    }
 
-	// validating primary keyword on page title
-	$.each(primaryKeyword, function(index, keyword) {
-		if($("title")[0].innerHTML.indexOf(keyword) == -1) {
-			messages.push("Primary keyword [ " + keyword + " ] does not exist in the title")
-		}
+    // validating primary keyword on page title
+    $.each(primaryKeyword, function (index, keyword) {
+        if ($("title")[0].innerHTML.indexOf(keyword) == -1) {
+            messages.push("Primary keyword [ " + keyword + " ] does not exist in the title")
+        }
 
 
-		if(($("title")[0].innerHTML.match(new RegExp(keyword, "gi")) || []).length > 1) {
-			messages.push("Primary keyword [ " + keyword + " ] can't be used more than onces")
-		}
-	})
+        if (($("title")[0].innerHTML.match(new RegExp(keyword, "gi")) || []).length > 1) {
+            messages.push("Primary keyword [ " + keyword + " ] can't be used more than onces")
+        }
+    })
 
-	return messages
+    return messages
 }
 
 
@@ -58,17 +58,17 @@ function validateTitle(messages) {
  *
  */
 function validateMetaInformation(messages) {
-	var metaDescription = $('meta[name=description]').attr("content");
+    var metaDescription = $('meta[name=description]').attr("content");
 
-	if(metaDescription.length < 20 || metaDescription.length > 160) {
-		messages.push("Meta description should be between 20 character to 160 character")
-	}
+    if (metaDescription.length < 20 || metaDescription.length > 160) {
+        messages.push("Meta description should be between 20 character to 160 character")
+    }
 
-	if( (metaDescription.match(/"/g) || []).length > 0) {
-		messages.push("Can't include double quotation marks in the meta description")
-	}
+    if ((metaDescription.match(/"/g) || []).length > 0) {
+        messages.push("Can't include double quotation marks in the meta description")
+    }
 
-	return messages
+    return messages
 }
 
 /**
@@ -78,17 +78,17 @@ function validateMetaInformation(messages) {
  */
 function validatePageElement(messages) {
 
-		// validating h1 tag
-		if($("h1").length == 0) {
-			messages.push("Page element must have one h1 tag")
-		}
+    // validating h1 tag
+    if ($("h1").length == 0) {
+        messages.push("Page element must have one h1 tag")
+    }
 
-		// validating h2 tag
-		if($("h2").length == 0) {
-			messages.push("Page element having an h2 tag is better")
-		}
+    // validating h2 tag
+    if ($("h2").length == 0) {
+        messages.push("Page element having an h2 tag is better")
+    }
 
-		return messages
+    return messages
 }
 
 /**
@@ -98,20 +98,20 @@ function validatePageElement(messages) {
  */
 function validateImage(messages) {
 
-	$.each($("img"), function(index, imageTag) {
+    $.each($("img"), function (index, imageTag) {
 
-		// validating img alt tag
-		if(typeof $(this).attr('alt') == "undefined") {
-			messages.push("Image source [ " + $(this).attr('src') + " ] must have alt tag")
-		}
+        // validating img alt tag
+        if (typeof $(this).attr('alt') == "undefined") {
+            messages.push("Image source [ " + $(this).attr('src') + " ] must have alt tag")
+        }
 
-		// validating img alt tag length
-		if( (typeof $(this).attr('alt') != "undefined") && ($(this).attr('alt').match(/\S+/g).length < 4)) {
-			messages.push("Image source [ " + $(this).attr('src') + " ] alt tag must be > 3 words")
-		}
-	})
+        // validating img alt tag length
+        if ((typeof $(this).attr('alt') != "undefined") && ($(this).attr('alt').match(/\S+/g).length < 4)) {
+            messages.push("Image source [ " + $(this).attr('src') + " ] alt tag must be > 3 words")
+        }
+    })
 
-	return messages
+    return messages
 }
 
 /**
@@ -121,21 +121,21 @@ function validateImage(messages) {
  */
 function validateSchema(messages) {
 
-	// validating facebook graph meta
-	if($("meta[name*='og:']").length > 0) {
-		messages.push("Markup: Facebook graph is good")
-	} else {
-		messages.push("Markup: It'll be better if you add facebook graph")
-	}
+    // validating facebook graph meta
+    if ($("meta[name*='og:']").length > 0) {
+        messages.push("Markup: Facebook graph is good")
+    } else {
+        messages.push("Markup: It'll be better if you add facebook graph")
+    }
 
-	// validatin twitter card
-	if($("meta[name*='twitter:']").length > 0) {
-		messages.push("Markup: Twitter card is good")
-	} else {
-		messages.push("Markup: It'll be better if you add twitter card")
-	}
+    // validatin twitter card
+    if ($("meta[name*='twitter:']").length > 0) {
+        messages.push("Markup: Twitter card is good")
+    } else {
+        messages.push("Markup: It'll be better if you add twitter card")
+    }
 
-	return messages
+    return messages
 }
 
 /**
@@ -144,15 +144,15 @@ function validateSchema(messages) {
  *
  */
 function validateCanonical(messages) {
-	if($("link[rel='canonical']").length == 0) {
-		messages.push("Please add canonical link")
-	}
+    if ($("link[rel='canonical']").length == 0) {
+        messages.push("Please add canonical link")
+    }
 
-	if( ($("link[rel='canonical']").length > 0) && !$("link[rel='canonical']").attr("href") ) {
-		messages.push("Canonical link can't be empty")
-	}
+    if (($("link[rel='canonical']").length > 0) && !$("link[rel='canonical']").attr("href")) {
+        messages.push("Canonical link can't be empty")
+    }
 
-	return messages
+    return messages
 }
 
 /**
@@ -162,30 +162,30 @@ function validateCanonical(messages) {
  */
 function validateOnLoad(messages) {
 
-	window.onload = function() {
+    window.onload = function () {
 
-		// setup timer
-		var now = new Date().getTime();
-		var latency = now - start;
+        // setup timer
+        var now = new Date().getTime();
+        var latency = now - start;
 
-		var loadTime = ((latency % 60000) / 1000).toFixed(0);
+        var loadTime = ((latency % 60000) / 1000).toFixed(0);
 
-		// push on load message based on load time
-		if (loadTime < 1) {
-			messages.push("Best load time")
-		} else if (loadTime < 3) {
-			messages.push("Better load time")
-		} else if (loadTime < 6) {
-			messages.push("Good load time")
-		} else if (loadTime < 7) {
-			messages.push("Bad load time")
-		} else {
-			messages.push("Worst load time")
-		}
+        // push on load message based on load time
+        if (loadTime < 1) {
+            messages.push("Best load time")
+        } else if (loadTime < 3) {
+            messages.push("Better load time")
+        } else if (loadTime < 6) {
+            messages.push("Good load time")
+        } else if (loadTime < 7) {
+            messages.push("Bad load time")
+        } else {
+            messages.push("Worst load time")
+        }
 
-		// print out all validation error. always call it at the end of script
-		$.each(messages, function(index, message) {
-			console.log(message)
-		})
-	}
+        // print out all validation error. always call it at the end of script
+        $.each(messages, function (index, message) {
+            console.log(message)
+        })
+    }
 }
